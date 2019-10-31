541d3d259e255964b5bf9c8f635b6f23a6358fa5
ricerkare <dwarfmistress@gmail.com>
2019-10-31 15:08:27 -0500

    Simlified Player class.

--- hotline_zombie/src/hotline_zombie/Player.java
+++ hotline_zombie/src/hotline_zombie/Player.java
@@ -7,12 +7,14 @@ import java.awt.Rectangle;
 //This class describes the player actions (movement, etc.)
 public class Player extends Object
 {
+	int moveIncr;
 	Object_Handler oHandler; //The handler will tell us the key inputs that were given to it and the player object will move in accordance to those inputs
-	public Player(int x, int y, Object_Type type, Object_Handler oHandler)
+	public Player(int x, int y, int moveIncr, Object_Type type, Object_Handler oHandler)
 	{
 		super(x, y, type);
 		xVelocity = 0; //He does not move for now (We will add key events/event handlers later)
 		yVelocity = 0;
+		this.moveIncr = moveIncr; 
 		this.oHandler = oHandler;
 	}
 	
@@ -23,68 +25,8 @@ public class Player extends Object
 		
 		//All key combinations here
 		
-		//Only Upward movement
-		if(oHandler.isUp() && !oHandler.isLeft() && !oHandler.isRight() && !oHandler.isDown())
-		{
-			yVelocity = -5;
-			xVelocity = 0;
-		}
-	
-		//Only Downward movement
-		if(!oHandler.isUp() && !oHandler.isLeft() && !oHandler.isRight() && oHandler.isDown())
-		{
-			yVelocity = 5;
-			xVelocity = 0;
-		}
-		
-		//Only Leftward movement
-		if(!oHandler.isUp() && oHandler.isLeft() && !oHandler.isRight() && !oHandler.isDown())
-		{
-			yVelocity = 0;
-			xVelocity = -5;
-		}		
-		
-		//Only Rightward movement
-		if(!oHandler.isUp() && !oHandler.isLeft() && oHandler.isRight() && !oHandler.isDown())
-		{
-			yVelocity = 0;
-			xVelocity = 5;
-		}
-		
-		//No Keys
-		if(!oHandler.isUp() && !oHandler.isLeft() && !oHandler.isRight() && !oHandler.isDown())
-		{
-			yVelocity = 0;
-			xVelocity = 0;
-		}
-		
-		//Upward Left Diagonal Movement
-		if(oHandler.isUp() && oHandler.isLeft() && !oHandler.isRight() && !oHandler.isDown())
-		{
-			yVelocity = -5;
-			xVelocity = -5;
-		}
-		
-		//Upward Right Diagonal Movement
-		if(oHandler.isUp() && !oHandler.isLeft() && oHandler.isRight() && !oHandler.isDown())
-		{
-			yVelocity = -5;
-			xVelocity = 5;
-		}
-		
-		//Downward Left Diagonal Movement
-		if(!oHandler.isUp() && oHandler.isLeft() && !oHandler.isRight() && oHandler.isDown())
-		{
-			yVelocity = 5;
-			xVelocity = -5;
-		}
-		
-		//Downward Right Diagonal Movement
-		if(!oHandler.isUp() && !oHandler.isLeft() && oHandler.isRight() && oHandler.isDown())
-		{
-			yVelocity = 5;
-			xVelocity = 5;
-		}
+		xVelocity = ((oHandler.isRight() ? 1 : 0) - (oHandler.isLeft() ? 1 : 0)) * moveIncr;
+		yVelocity = ((oHandler.isDown() ? 1 : 0) - (oHandler.isUp() ? 1 : 0)) * moveIncr;
 		
 	}
 	
